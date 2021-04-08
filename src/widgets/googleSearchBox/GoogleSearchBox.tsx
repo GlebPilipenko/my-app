d14c9538f3b22dc7358ba84f049929c967cebd63
g.pilipenko <Tonia_300987+>
2021-04-08 14:46:52 +0300

    googleSearchBox - refactoring GoogleSearchBox.tsx, delete debugger and some code

--- src/widgets/googleSearchBox/GoogleSearchBox.tsx
+++ src/widgets/googleSearchBox/GoogleSearchBox.tsx
@@ -2,15 +2,9 @@ import {useEffect} from 'react';
 import {useInputValue} from 'src/hooks';
 import {searchBoxAutocomplete} from './utils';
 import style from './GoogleSearchBox.module.css';
-import {getParseLocalStorageData} from '../../services';
-import {LocalStorageTitles} from './enums';
 
 export const GoogleSearchBox = () => {
   const [value, , changeInputValue] = useInputValue('');
-  const googleSearchBoxWidget = LocalStorageTitles.GoogleSearchBox;
-  const dataFromLocalStorage = getParseLocalStorageData(googleSearchBoxWidget);
-
-  debugger
 
   useEffect(() => {
     searchBoxAutocomplete();
@@ -25,12 +19,6 @@ export const GoogleSearchBox = () => {
         onChange={changeInputValue}
         className={style.search__box}
       />
-      {dataFromLocalStorage.map((el: any) => {
-        debugger
-        return el.photos?.map((el: any) => {
-          return el.html_attributions
-        })
-      })}
     </div>
   );
 };
