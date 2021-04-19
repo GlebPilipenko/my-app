01dfbbb828d7036390dfa7e14f2329d0f5866ca3
g.pilipenko <Tonia_300987+>
2021-04-19 17:57:56 +0300

    formValidation - refactoring code style

--- src/hooks/typings/index.ts
+++ src/hooks/typings/index.ts
@@ -1,8 +1,8 @@
 import {ChangeEvent} from 'react';
 
+export type ReturnUseVisibilityFormType = [
+  boolean, (value: boolean) => void, () => void
+];
+
 export type ReturnUseInputValueType = [number, string, (value: string) => void,
   (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void];
-
-export type ReturnUseVisibilityFormType = [
-  boolean, (value: boolean) => void ,() => void
-];
